fa788598a42a018e7bcb5197d96aee1442d3970e
Rana Saad Imran <22100018@lums.edu.pk>
2023-03-20 17:33:36 +0500

    Update resnet18.py

--- resnet18.py
+++ resnet18.py
@@ -1,4 +1,7 @@
 from libraries import *
 
-
 # ResNet18 model
+ResNet18 = torchvision.models.resnet18(pretrained=True)
+ResNet18 = ResNet18.to(device) 
+criterion = nn.CrossEntropyLoss()  #(set loss function)
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
