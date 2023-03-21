eaa6a760296ed70294279dd82ad5c2fc3a879363
Rana Saad Imran <22100018@lums.edu.pk>
2023-03-21 13:21:19 +0500

    Update resnet18.py

--- resnet18.py
+++ resnet18.py
@@ -1,12 +1,12 @@
 from libraries import *
 from torch.optim.lr_scheduler import StepLR
 
-# ResNet18 (pretrained) model
-ResNet18 = torchvision.models.resnet18(pretrained=True)
-ResNet18 = ResNet18.to(device) 
+# ResNet18 model
+model = torchvision.models.resnet18(weights='ResNet18_Weights.IMAGENET1K_V1')
+ResNet18 = model.to(device) 
 
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()  #(set loss function)
-optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+optimizer = optimizer.SGD(model.parameters(), lr=0.001, momentum=0.9)
 # scheduler for dynamically adjusting the learning rate
 scheduler = StepLR(optimizer, step_size=2,gamma=0.1)
